cb7962f0a6d3072520943a41fa14fdef88623954
Michael Hardie <hardie.michael63@gmail.com>
2023-10-07 21:33:36 +1030

    particles fade in when audio detected by mic

--- check.js
+++ check.js
@@ -23,46 +23,22 @@ async function setup() {
     // Create the particle system
     geometry = new THREE.BufferGeometry();
     const vertices = [];
-    for (let i = 0; i < MAX_PARTICLES; i++) {
+    for (let i = 0; i < 1000; i++) {
         const x = (Math.random() - 0.5) * 200;
         const y = (Math.random() - 0.5) * 200;
         const z = (Math.random() - 0.5) * 200;
         vertices.push(x, y, z);
     }
     geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-
-    const alphas = new Float32Array(MAX_PARTICLES).fill(0);
-    geometry.setAttribute('alpha', new THREE.Float32BufferAttribute(alphas, 1));
-
-    material = new THREE.ShaderMaterial({
-        uniforms: {
-            color: { value: new THREE.Color(0xffffff) }
-        },
-        vertexShader: `
-            attribute float alpha;
-            varying float vAlpha;
-            void main() {
-                vAlpha = alpha;
-                gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-                gl_PointSize = 1.0;
-            }
-        `,
-        fragmentShader: `
-            uniform vec3 color;
-            varying float vAlpha;
-            void main() {
-                gl_FragColor = vec4(color, vAlpha);
-            }
-        `,
-        transparent: true
-    });
-
+    material = new THREE.PointsMaterial({ size: 1, color: 0xffffff });
     particles = new THREE.Points(geometry, material);
     scene.add(particles);
+
     camera.position.z = 5;
 
     // Tone.js setup for audio input
     micStream = new Tone.UserMedia();
+
     try {
         await micStream.open();
         analyser = new Tone.Analyser("waveform", 1024);
@@ -79,27 +55,18 @@ function animate() {
 
     data = analyser.getValue();
     const avgAmplitude = data.reduce((acc, val) => acc + Math.abs(val), 0) / data.length;
-    const alphas = particles.geometry.attributes.alpha.array;
 
     if (avgAmplitude > LOUDNESS_THRESHOLD) {
         spawnParticles();
-        for (let i = 0; i < MAX_PARTICLES; i++) {
-            alphas[i] = Math.min(1.0, alphas[i] + 0.05);  // Increase opacity
-        }
-    } else {
-        for (let i = 0; i < MAX_PARTICLES; i++) {
-            alphas[i] *= 0.95;  // Decrease opacity over time
-        }
     }
 
-    particles.geometry.attributes.alpha.needsUpdate = true;
     particles.geometry.attributes.position.needsUpdate = true;
     renderer.render(scene, camera);
-    
 }
 
 function spawnParticles() {
     const positions = particles.geometry.attributes.position.array;
+
     for (let i = 0; i < MAX_PARTICLES; i += 3) {
         if (Math.random() < 0.1) { // Probability to spawn a new particle
             positions[i] = (Math.random() - 0.5) * 200; // x
@@ -119,6 +86,5 @@ window.addEventListener('resize', () => {
     camera.aspect = width / height;
     camera.updateProjectionMatrix();
 });
-
 
 setup();
